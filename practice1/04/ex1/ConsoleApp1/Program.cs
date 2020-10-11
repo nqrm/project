18edfe7a8efed4f0fb11447b4f749295a86e5b4d
ppunnk zxc <vladislav.kevorkov@gmail.com>
2020-10-11 22:33:40 +0300

    ex2

---
+++ practice1/04/ex1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int16 i16 = 1; 
+            Int32 i32 = 1; 
+            double db = 1;
+
+            i16 = (short)i32;
+            i16 = (short)db;
+            i32 = i16;
+            i32 = (int)db;
+            db = i16;
+            db = i32;
+        }
+    }
+}
