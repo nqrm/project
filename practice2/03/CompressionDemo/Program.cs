dc328517489b19e8ea55afbbe666ad6ed516ebb9
ppunnk zxc <vladislav.kevorkov@gmail.com>
2020-10-13 22:06:25 +0300

    03 ex1 and ex2

---
+++ practice2/03/CompressionDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace CompressionDemo
+{
+    class Program
+    {
+        /*static void CompressFile(string inFilename, string outFilename)
+        {
+            FileStream sourceFile = File.OpenRead(inFilename);
+            FileStream destFile = File.Create(outFilename);
+            GZipStream compStream = new GZipStream(destFile, CompressionMode.Compress);
+            int theByte = sourceFile.ReadByte();
+           while (theByte != -1)
+            {
+                compStream.WriteByte((byte)theByte); theByte = sourceFile.ReadByte();
+            }
+            compStream.Close();
+        }*/
+
+        static void UncompressFile(string inFilename, string outFilename)
+        {
+            FileStream sourceFile = File.OpenRead(inFilename);
+            FileStream destFile = File.Create(outFilename);
+            GZipStream compStream = new GZipStream(sourceFile, CompressionMode.Decompress);
+            int theByte = compStream.ReadByte();
+            while (theByte != -1)
+            {
+                destFile.WriteByte((byte)theByte);
+                theByte = compStream.ReadByte();
+            }
+            compStream.Close();
+        }
+            static void Main(string[] args)
+        {
+            //CompressFile(@"D:\newfile.txt", @"D:\newfile.txt.gz");
+            UncompressFile(@"D:\newfile.txt.gz", @"D:\newfile1.txt");
+        }
+
+    }
+}
