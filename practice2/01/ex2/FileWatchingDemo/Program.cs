d67aa4733e3a837983da190de0a18e24f712725f
ppunnk zxc <vladislav.kevorkov@gmail.com>
2020-10-13 20:53:53 +0300

    01 ex2

---
+++ practice2/01/ex2/FileWatchingDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace FileWatchingDemo
+{
+    class Program
+    {
+    static void watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("Changed: {0}", e.FullPath);
+        }
+        static void Main(string[] args)
+        {
+            FileSystemWatcher watcher = new FileSystemWatcher(Environment.SystemDirectory);
+            watcher.Filter = "*.ini";
+            watcher.IncludeSubdirectories = true;
+            watcher.EnableRaisingEvents = true;
+            watcher.NotifyFilter = NotifyFilters.Attributes | NotifyFilters.Size;
+            watcher.Changed += new FileSystemEventHandler(watcher_Changed);
+        }
+    }
+}
