1535e20eaf9902dc8e80557ce58ab0f0e6565217
ppunnk zxc <vladislav.kevorkov@gmail.com>
2020-10-13 21:03:24 +0300

    02 ex1 and ex2

---
+++ practice2/02/FileDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace FileDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter writer = File.CreateText(@"D:\newfile.txt");
+            writer.WriteLine("This is my new file");
+            writer.WriteLine("Do you like its format?");
+            writer.Close();
+
+            StreamReader reader = File.OpenText(@"D:\newfile.txt");
+            string contents = reader.ReadToEnd();
+            reader.Close();
+            Console.WriteLine(contents);
+        }
+    }
+}
