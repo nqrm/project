191bb0c348461d0606272c7679f25afbf835a5dc
ppunnk zxc <vladislav.kevorkov@gmail.com>
2020-10-13 20:18:21 +0300

    01 ex1

---
+++ practice2/01/ex1/ShowFilesDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace ShowFilesDemo
+{
+    class Program
+    {
+        static void ShowDirectory(DirectoryInfo dir)
+        {
+            try
+            {
+                foreach (FileInfo file in dir.GetFiles())
+                {
+                    Console.WriteLine("File: {0}", file.FullName);
+                }
+
+                foreach (DirectoryInfo subDir in dir.GetDirectories())
+                {
+                    ShowDirectory(subDir);
+                }
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                Console.WriteLine("Для доступа к папке нужны права администратора");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(Environment.SystemDirectory);
+            ShowDirectory(dir);
+        }
+    }
+}
+
