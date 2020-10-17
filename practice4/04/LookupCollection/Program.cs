92487ffa7bd4114a96699f68888f7c4e34dbf5f7
ppunnk zxc <vladislav.kevorkov@gmail.com>
2020-10-17 18:56:55 +0300

    04 ex1

---
+++ practice4/04/LookupCollection/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Globalization;
+
+namespace LookupCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListDictionary list = new ListDictionary(new CaseInsensitiveComparer(CultureInfo.InvariantCulture));
+            list["Estados Unidos"] = "United States of America";
+            list["Canada"] = "Canada";
+            list["Espana"] = "Spain";
+            Console.WriteLine(list["espana"]);
+            Console.WriteLine(list["CANADA"]);
+            Console.Read();
+        }
+    }
+}
